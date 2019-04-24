8a6875077dc8c5d942eb1470a1c32b354c346cee
david andresen <dandresen01@gmail.com>
2019-04-24 16:47:23 -0500

    Added andresen-work SG to instance

--- main.tf
+++ main.tf
@@ -7,16 +7,16 @@ provider "aws" {
 }
 
 # vpc
-resource "aws_vpc" "helloVpc" {
-  cidr_block = "10.0.0.0/16"
-}
+# resource "aws_vpc" "helloVpc" {
+#   cidr_block = "10.0.0.0/16"
+# }
 
 # public ec2
 resource "aws_instance" "web" {
   ami               = "ami-0cd3dfa4e37921605"
   instance_type     = "t2.micro"
-  security_group_id = "${aws_security_group.dmz_2.id}"
-  key_name          = "andresen"
+  #security_group_id = "${aws_security_group.dmz_2.id}"
+  key_name          = "andresen-work"
 
   tags = {
     Name = "Web EC2"
@@ -27,44 +27,40 @@ resource "aws_instance" "web" {
   }
 }
 
-# resource "aws_key_pair" "deployer" {
-#   key_name = "${var.key_name}"
-# }
-
 # resource "vpc" "my-vpc" {
 #   aws_region = ""
 #   cidr_block = ""
 # }
 
 ### Security Group- dmz_2
-resource "aws_security_group" "dmz_2" {
-  name        = "dmz-2"
-  description = "SG for DMZ"
-
-  tags {
-    "Name" = "dmz_2"
-  }
-
-  resource "aws_security_group_rule" "dmz_egress_all" {
-    type              = "egress"
-    protocol          = "all"
-    from_port         = 0
-    to_port           = 0
-    cidr_blocks       = ["0.0.0.0/0"]
-    security_group_id = "${aws_security_group.dmz_2.id}"
-    description       = "All outbound traffic"
-  }
-
-  resource "aws_security_group_rule" "dmz_ingress_ssh" {
-    type              = "ingress"
-    protocol          = "tcp"
-    from_port         = 22
-    to_port           = 22
-    cidr_blocks       = ["0.0.0.0/0"]
-    security_group_id = "${aws_security_group.dmz_2.id}"
-    description       = "SSH from anywhere"
-  }
-}
+# resource "aws_security_group" "dmz_2" {
+#   name        = "dmz-2"
+#   description = "SG for DMZ"
+#
+#   tags {
+#     "Name" = "dmz_2"
+#   }
+#
+#   resource "aws_security_group_rule" "dmz_egress_all" {
+#     type              = "egress"
+#     protocol          = "all"
+#     from_port         = 0
+#     to_port           = 0
+#     cidr_blocks       = ["0.0.0.0/0"]
+#     security_group_id = "${aws_security_group.dmz_2.id}"
+#     description       = "All outbound traffic"
+#   }
+#
+#   resource "aws_security_group_rule" "dmz_ingress_ssh" {
+#     type              = "ingress"
+#     protocol          = "tcp"
+#     from_port         = 22
+#     to_port           = 22
+#     cidr_blocks       = ["0.0.0.0/0"]
+#     security_group_id = "${aws_security_group.dmz_2.id}"
+#     description       = "SSH from anywhere"
+#   }
+# }
 
 # resource "aws_security_group_rule" "sg_allow_ssh" {
 #   # dmz  # create one for private subnet
@@ -78,4 +74,3 @@ resource "aws_security_group" "dmz_2" {
 # resource "aws_route_table" "my-rt" {}
 #
 # resource "aws_nat_gateway" "my-nat-gtwy" {}
-
--- variables.tf
+++ variables.tf
@@ -4,6 +4,3 @@ variable "secret_key" {}
 variable "region" {
   default = "us-east-2"
 }
-
-# variable "key_name" {}
-
