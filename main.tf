ed2721ea24d52de31a8fb9696f761f31ff9f9e7e
dandresen <dandresen01@gmail.com>
2019-04-25 05:40:13 -0500

    Created VPC with one EC2 instance. RT still buggy

--- main.tf
+++ main.tf
@@ -7,19 +7,69 @@ provider "aws" {
 }
 
 # vpc
-# resource "aws_vpc" "helloVpc" {
-#   cidr_block = "10.0.0.0/16"
-# }
+resource "aws_vpc" "mainVpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+# internet gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = "${aws_vpc.mainVpc.id}"
+}
+
+# this is not the main route table
+# need to associate the pub subnet with the main rt
+# use below to associate priv sub with nat 
+# main route table
+resource "aws_route_table" "mainRt" {
+  vpc_id = "${aws_vpc.mainVpc.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.gw.id}"
+
+    # instance_id = "${aws_instance.web.id}"
+  }
+
+  tags {
+    Name = "VPC-RT"
+  }
+}
+
+# subnets
+# public subnet
+resource "aws_subnet" "public" {
+  vpc_id            = "${aws_vpc.mainVpc.id}"
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-2a"
+
+  tags = {
+    Name = "Public Subnet"
+  }
+}
+
+# private subnet
+resource "aws_subnet" "private" {
+  vpc_id            = "${aws_vpc.mainVpc.id}"
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-2b"
+
+  tags = {
+    Name = "Private Subnet"
+  }
+}
 
 # public ec2
 resource "aws_instance" "web" {
-  ami               = "ami-0cd3dfa4e37921605"
-  instance_type     = "t2.micro"
-  #security_group_id = "${aws_security_group.dmz_2.id}"
-  key_name          = "andresen-work"
+  depends_on                  = ["aws_internet_gateway.gw"]
+  ami                         = "ami-0cd3dfa4e37921605"
+  instance_type               = "t2.micro"
+  key_name                    = "${var.key_name}"
+  vpc_security_group_ids      = ["${aws_security_group.dmz_2.id}"]
+  subnet_id                   = "${aws_subnet.public.id}"
+  associate_public_ip_address = true
 
   tags = {
-    Name = "Web EC2"
+    Name = "Web Terraform EC2"
   }
 
   provisioner "local-exec" {
@@ -27,50 +77,27 @@ resource "aws_instance" "web" {
   }
 }
 
-# resource "vpc" "my-vpc" {
-#   aws_region = ""
-#   cidr_block = ""
-# }
+# Security Group- dmz_2
+resource "aws_security_group" "dmz_2" {
+  name        = "dmz-2"
+  description = "SG for DMZ"
+  vpc_id      = "${aws_vpc.mainVpc.id}"
 
-### Security Group- dmz_2
-# resource "aws_security_group" "dmz_2" {
-#   name        = "dmz-2"
-#   description = "SG for DMZ"
-#
-#   tags {
-#     "Name" = "dmz_2"
-#   }
-#
-#   resource "aws_security_group_rule" "dmz_egress_all" {
-#     type              = "egress"
-#     protocol          = "all"
-#     from_port         = 0
-#     to_port           = 0
-#     cidr_blocks       = ["0.0.0.0/0"]
-#     security_group_id = "${aws_security_group.dmz_2.id}"
-#     description       = "All outbound traffic"
-#   }
-#
-#   resource "aws_security_group_rule" "dmz_ingress_ssh" {
-#     type              = "ingress"
-#     protocol          = "tcp"
-#     from_port         = 22
-#     to_port           = 22
-#     cidr_blocks       = ["0.0.0.0/0"]
-#     security_group_id = "${aws_security_group.dmz_2.id}"
-#     description       = "SSH from anywhere"
-#   }
-# }
+  egress {
+    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-# resource "aws_security_group_rule" "sg_allow_ssh" {
-#   # dmz  # create one for private subnet
-# }
-#
-# resource "aws_security_group_rule" "sg_allow_http" {
-#   # dmz  # create one for private subnet
-# }
+  ingress {
+    protocol    = "tcp"
+    from_port   = 22
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-
-# resource "aws_route_table" "my-rt" {}
-#
-# resource "aws_nat_gateway" "my-nat-gtwy" {}
+  tags {
+    Name = "dmz_2"
+  }
+}
--- variables.tf
+++ variables.tf
@@ -4,3 +4,7 @@ variable "secret_key" {}
 variable "region" {
   default = "us-east-2"
 }
+
+variable "key_name" {
+  description = "Name of existing key pair to SSH into instances."
+}
